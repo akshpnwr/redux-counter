f7d29a19d1f653009f0c9edd57b2a75338a8c770
Akash Panwar <akashpanwar9976@gmail.com>
2022-09-19 18:20:59 +0530

    working with multiple state properties

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,23 +3,31 @@ import classes from './Counter.module.css';
 
 const Counter = () => {
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
   const dispatch = useDispatch();
 
   const incrementHandler = () => {
-    dispatch({ type: 'INCREMENT' });
+    dispatch({ type: 'INCREMENT', amount: 1 });
+  };
+
+  const increaseHandler = () => {
+    dispatch({ type: 'INCREASE', amount: 5 });
   };
   const decrementHandler = () => {
     dispatch({ type: 'DECREMENT' });
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: 'TOGGLE' });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,37 @@
 const { configureStore } = require('@reduxjs/toolkit');
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initialState, action) => {
+  if (action.type == 'INCREASE') {
+    return {
+      counter: state.counter + action.amount,
+      showCounter: state.showCounter,
+    };
+  }
+
   if (action.type == 'INCREMENT') {
     return {
       counter: state.counter + 1,
+      showCounter: state.showCounter,
     };
   }
 
   if (action.type == 'DECREMENT') {
     return {
       counter: state.counter - 1,
+      showCounter: state.showCounter,
     };
   }
 
-  return {
-    counter: 0,
-  };
+  if (action.type == 'TOGGLE') {
+    return {
+      counter: state.counter,
+      showCounter: !state.showCounter,
+    };
+  }
+
+  return initialState;
 };
 
 const store = configureStore({
